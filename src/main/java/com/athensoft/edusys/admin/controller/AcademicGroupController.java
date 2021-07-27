142052f3f2bbca465de299fc3bf1759f794c65b7
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-07-27 13:47:49 -0400

    added initial implementation of AcademicGroup retrieval

--- src/main/java/com/athensoft/edusys/admin/controller/AcademicGroupController.java
+++ src/main/java/com/athensoft/edusys/admin/controller/AcademicGroupController.java
@@ -1,10 +1,15 @@
 package com.athensoft.edusys.admin.controller;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.athensoft.edusys.admin.entity.AcademicGroup;
@@ -20,15 +25,33 @@ public class AcademicGroupController {
 	}
 	
 	@GetMapping("/groups")
-	public List<AcademicGroup> getDataList(){
-		return acdGroupService.getAcademicGroupList();
+	public ResponseEntity<List<AcademicGroup>> getDataList(){
+		return ResponseEntity.ok(acdGroupService.getAcademicGroupList());
 	}
 	
 	@GetMapping("/groups/{groupId}")
-	public AcademicGroup getDataById(@PathVariable Integer groupId) {
-		return acdGroupService.getAcademicGroupById(groupId);
+	public ResponseEntity<AcademicGroup> getDataById(@PathVariable Integer groupId) {
+		return ResponseEntity.ok(acdGroupService.getAcademicGroupById(groupId));
 	}
 	
+	@GetMapping("/groups/filters")
+	public ResponseEntity<List<AcademicGroup>> getDataByFilters(
+			@RequestParam(name = "groupId") Optional<Integer> groupId, 
+			@RequestParam(name = "groupNo", required = false) String groupNo,
+			@RequestParam(name = "groupStatus") Optional<Integer> groupStatus,
+			@RequestParam(name = "startDate") Optional<String> startDate,
+			@RequestParam(name = "endDate") Optional<String> endDate,
+			@RequestParam(name = "sessionNum") Optional<Integer> sessionNum, 
+			@RequestParam(name = "groupType") Optional<Integer> groupType,
+			@RequestParam(name = "groupName", required = false) String groupName,
+			@RequestParam(name = "groupDesc", required = false) String groupDesc) throws ParseException{
+		return ResponseEntity.ok(acdGroupService.getAcademicGroupListByFilters(groupId, groupNo, groupStatus, startDate, endDate, sessionNum, groupType, groupName, groupDesc));
+	}
+	
+	@GetMapping("/groups/filtersStr")
+	public ResponseEntity<List<AcademicGroup>> getDataByFiltersStr() throws ParseException{
+		return ResponseEntity.ok(acdGroupService.getAcademicGroupListByFiltersStr());
+	}
 
 
 }
--- src/main/java/com/athensoft/edusys/admin/service/AcademicGroupService.java
+++ src/main/java/com/athensoft/edusys/admin/service/AcademicGroupService.java
@@ -1,15 +1,29 @@
 package com.athensoft.edusys.admin.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import com.athensoft.edusys.admin.dao.AcademicGroupRepository;
 import com.athensoft.edusys.admin.entity.AcademicGroup;
+import com.athensoft.edusys.admin.entity.GroupStatus;
+import com.athensoft.edusys.admin.entity.GroupType;
 import com.athensoft.edusys.error.exceptions.AcademicGroupNotFoundException;
+import com.athensoft.edusys.utils.validation.GlobalValidationUtils;
 
 @Service
 public class AcademicGroupService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(AcademicGroupService.class);
 	private final AcademicGroupRepository acdGroupRepo;
 	
 	public AcademicGroupService(AcademicGroupRepository acdGroupRepo) {
@@ -23,5 +37,171 @@ public class AcademicGroupService {
 	public AcademicGroup getAcademicGroupById(Integer groupId) {
 		return acdGroupRepo.findById(groupId).orElseThrow(() -> new AcademicGroupNotFoundException(groupId));
 	}
+	
+	public List<AcademicGroup> getAcademicGroupListByFilters(
+			Optional<Integer> groupId, 
+			String groupNo, 
+			Optional<Integer> groupStatus, 
+			Optional<String> startDateStr, 
+			Optional<String> endDateStr, 
+			Optional<Integer> sessionNum, 
+			Optional<Integer> groupType, 
+			String groupName, 
+			String groupDesc
+			) throws ParseException{
+		List<String> ignoredProperties = new ArrayList<>();
+		AcademicGroup group = new AcademicGroup();
+		if(groupId.isEmpty()) {
+			ignoredProperties.add("groupId");
+		}else {
+			group.setGroupId(groupId.get());
+		}
+		
+		if(groupStatus.isEmpty()) {
+			ignoredProperties.add("groupStatus");
+		}else {
+			group.setGroupStatus(GroupStatus.values()[groupStatus.get()]);
+		}
+		
+		if(startDateStr.isEmpty()) {
+			ignoredProperties.add("startDate");
+		}else {
+			Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDateStr.get());
+			group.setStartDate(startDate);
+		}
+		
+		if(endDateStr.isEmpty()) {
+			ignoredProperties.add("endDate");
+		}else {
+			Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse(endDateStr.get());
+			group.setEndDate(endDate);
+		}
+		
+		if(sessionNum.isEmpty()) {
+			ignoredProperties.add("sessionNum");
+		}else {
+			group.setSessionNum(sessionNum.get());
+		}
+		
+		if(groupType.isEmpty()) {
+			ignoredProperties.add("groupType");
+		}else {
+			group.setGroupType(GroupType.values()[groupType.get()]);
+		}
+		
+		group.setGroupNo(groupNo);
+		group.setGroupName(groupName);
+		group.setGroupDesc(groupDesc);
+		
+		LOGGER.debug("searched group:" + group);
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll();
+		if (!ignoredProperties.isEmpty()) {
+			exampleMatcher = exampleMatcher.withIgnorePaths(ignoredProperties.toArray(new String[0]));
+		}
+	
+		Example<AcademicGroup> example = Example.of(group, exampleMatcher);
+		LOGGER.debug("example group:" + example.toString());
+		return acdGroupRepo.findAll(example);
+		
+	}
+
+	public List<AcademicGroup> getAcademicGroupListByFiltersStr() throws ParseException {
+		JSONObject obj = new JSONObject();
+		obj.put("groupId", "1");
+		obj.put("groupNo", "");
+		obj.put("groupStatus", "");
+		obj.put("startDate", "");
+		obj.put("endDate", "");
+		obj.put("sessionNum", "");
+		obj.put("groupType", "2");
+		obj.put("groupName", "");
+		obj.put("groupDesc", "");
+		
+		String filterStr = obj.toString();
+		JSONObject jobj = new JSONObject(filterStr);
+		
+		List<String> ignoredProperties = new ArrayList<>();
+		String groupIdStr = jobj.getString("groupId").trim();
+		String groupNo = jobj.getString("groupNo").trim();
+		
+		String groupStatusStr = jobj.getString("groupStatus").trim();
+		String startDateStr = jobj.getString("startDate").trim();
+		String endDateStr = jobj.getString("endDate").trim();
+		String sessionNum = jobj.getString("sessionNum").trim();
+		String groupTypeStr = jobj.getString("groupType").trim();
+		String groupName = jobj.getString("groupName").trim(); 
+		String groupDesc = jobj.getString("groupDesc").trim();
+		
+		AcademicGroup group = new AcademicGroup();
+		
+		if (GlobalValidationUtils.isEmptyStr(groupIdStr)) {
+			ignoredProperties.add("groupId");
+		}else {
+			group.setGroupId(Integer.valueOf(groupIdStr));
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(groupNo)) {
+			ignoredProperties.add("groupNo");
+		}else {
+			group.setGroupNo(groupNo);
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(groupStatusStr)) {
+			ignoredProperties.add("groupStatus");
+		}else {
+			GroupStatus groupStatus = GroupStatus.values()[Integer.valueOf(groupStatusStr)];
+			group.setGroupStatus(groupStatus);
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(startDateStr)) {
+			ignoredProperties.add("startDate");
+		}else {
+			Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDateStr);
+			group.setStartDate(startDate);
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(endDateStr)) {
+			ignoredProperties.add("endDate");
+		}else {
+			Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse(endDateStr);
+			group.setEndDate(endDate);
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(sessionNum)) {
+			ignoredProperties.add("sessionNum");
+		}else {
+			group.setSessionNum(Integer.valueOf(sessionNum));
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(groupTypeStr)) {
+			ignoredProperties.add("groupType");
+		}else {
+			GroupType groupType = GroupType.values()[Integer.valueOf(groupTypeStr)];
+			group.setGroupType(groupType);
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(groupName)) {
+			ignoredProperties.add("groupName");
+		}else {
+			group.setGroupName(groupName);
+		}
+		
+		if (GlobalValidationUtils.isEmptyStr(groupDesc)) {
+			ignoredProperties.add("groupDesc");
+		}else {
+			group.setGroupDesc(groupDesc);
+		}
+		
+		LOGGER.debug("searched group:" + group);
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll();
+		if (!ignoredProperties.isEmpty()) {
+			exampleMatcher = exampleMatcher.withIgnorePaths(ignoredProperties.toArray(new String[0]));
+		}
+	
+		Example<AcademicGroup> example = Example.of(group, exampleMatcher);
+		LOGGER.debug("example group:" + example.toString());
+		return acdGroupRepo.findAll(example);
+	}
+	
 
 }
